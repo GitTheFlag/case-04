7c4e0d59dfbd52f7b284a826af8c31eb5eeb8e96
mapiayg <mapiayg@uoc.edu>
2026-03-24 20:45:47 +0100

    fix: restore >= in checkVersion to fix version comparison bug

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@
 function checkVersion() {
   const minimum = 18;
   const current = 18; // Node.js major version
-  return current > minimum; // bug still present
+  return current >= minimum; // fixed: use >= not >
 }
 
 function formatError(msg) {
