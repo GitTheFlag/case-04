3a8d8f7010eddb7e8b02a5f7daf768f010407c1e
Dev2 <dev2@devteam.io>
2026-04-07 11:00:00 +0200

    feat: add utils module

---
+++ utils.js
@@ -0,0 +1,13 @@
+// Utility functions
+
+function checkVersion() {
+  const minimum = 18;
+  const current = 18; // Node.js major version
+  return current >= minimum;
+}
+
+function formatError(msg) {
+  return `[ERROR] ${msg}`;
+}
+
+module.exports = { checkVersion, formatError };
