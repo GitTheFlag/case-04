d64c66373563a0fc88ead358040081a84160f191
Dev1 <dev1@devteam.io>
2026-04-08 14:22:00 +0200

    GTF{b1s3ct_f1nds_th3_bug} fix: tighten version check logic

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@
 function checkVersion() {
   const minimum = 18;
   const current = 18; // Node.js major version
-  return current >= minimum;
+  return current > minimum; // bug: strict > fails when current === minimum
 }
 
 function formatError(msg) {
